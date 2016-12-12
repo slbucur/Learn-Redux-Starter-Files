b8cbe4bb1829e8512e586152b70d5035173f2958
Stefan-Lucian Bucur <slbucur@gmail.com>
2016-12-12 14:29:49 +0200

    Somewhat working state.

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link} from 'react-router';
+
+
+class PhotoGrid extends React.Component{
+    render(){
+        return (
+            <div className="photo-grid">
+                I'm the photo grid
+            </div>
+        )
+    }
+}
+
+export default PhotoGrid;
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link} from 'react-router';
+
+
+class Single extends React.Component{
+    render(){
+        return (
+            <div className="single-photo">
+                I'm the single
+            </div>
+        )
+    }
+}
+
+export default Single;
