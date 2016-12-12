a5bc6a730c2e2a87d114982725116b1a900b1e32
Stefan-Lucian Bucur <slbucur@gmail.com>
2016-12-12 15:39:29 +0200

    Comment in the main page.

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -11,21 +11,20 @@ class Photo extends React.Component{
                     <Link to={`/view/${post.code}`}>
                           <img src={post.display_src} alt="" className="grid-photo"/>
                     </Link>
-                    <CSSTransitionGroup transition="like"
-                    transitionEnterTimeout={500}
-                    transitionLeaveTimeout={500}>
-                    <span key={post.likes} className="likes-heart"> {post.likes} </span>
+                    <CSSTransitionGroup transitionName="like" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+                        <span key={post.likes} className="likes-heart">{post.likes}</span>
                     </CSSTransitionGroup>
                 </div>
                 <figcaption>
                     <p> {post.caption} </p>
                     <div className="control-buttons">
-                        <button className="likes"> &hearts; {post.likes}</button>
+                        <button className="likes"
+                            onClick={this.props.increment.bind(null, i)}
+                        > &hearts; {post.likes}</button>
                         <Link to={`/view/${post.code}`}>
                             <span className="comment-count">
-                                <span className="speech-bubble">
-                                    {comments[post.code] ? comments[post.code].length : 0}
-                                </span>
+                            <span className="speech-bubble"></span>
+                            {comments[post.code] ? comments[post.code].length : 0 }
                             </span>
                         </Link>
                     </div>
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,7 +1,16 @@
 function posts(state = [], action){
-    console.log('The post is changed');
-    console.log(state, action);
-    return state;
+    switch (action.type){
+        case 'INCREMENT_LIKES':
+            console.log('Incrementing likes');
+            const i = action.index;
+            return [
+                ...state.slice(0, i), //before
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1) //after
+            ]
+        default:
+            return state;
+    }
 }
 
 export default posts;
