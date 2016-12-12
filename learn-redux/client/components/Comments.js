6c903f48b9aab9f0fcefc4e72476f1e4d2caa49d
Stefan-Lucian Bucur <slbucur@gmail.com>
2016-12-12 16:35:13 +0200

    Mock class for comments.

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link} from 'react-router';
+import Photo from './Photo'
+
+class Comments extends React.Component{
+    render(){
+        return (
+            <div className="comment">
+                Hello comment
+            </div>
+        )
+    }
+}
+
+export default Comments;
